d20842caa1cfc0a281fb01792de523d4b0b50da3
Cory M <cmarker4@gmail.com>
2021-01-14 22:33:36 -0600

    stuck

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
+require 'pry'
+
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id: nil, name:, type:, db:)
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    # binding.pry
+    sql = "INSERT INTO pokemon (name, type, db) VALUES (#{name}, #{type}, #{db})"
+    @id = ""
+
+    # DB[:conn].execute(sql, name, type, db)
+    # @id = DB[:conn].("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  end
 end
